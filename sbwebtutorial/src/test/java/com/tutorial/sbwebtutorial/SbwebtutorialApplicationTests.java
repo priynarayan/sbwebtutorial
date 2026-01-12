e644ecf58d50d80d69835275b4cb9ca20e9cc500
priynarayan <priyanshusingh5099@gmail.com>
2026-01-12 19:58:41 +0530

    Initial commit

---
+++ sbwebtutorial/src/test/java/com/tutorial/sbwebtutorial/SbwebtutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutorial.sbwebtutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbwebtutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
